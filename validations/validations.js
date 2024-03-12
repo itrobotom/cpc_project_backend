1e2ef515d1da6293fdcd7acba1f3b951ed48b156
itrobotom <russk1j@yandex.ru>
2024-03-12 22:58:05 +0700

    Поправлена валидация

--- validations/validations.js
+++ validations/validations.js
@@ -4,7 +4,7 @@ export const regValidation = [
     body('email', 'неверный формат почты').isEmail(),
     body('password', 'пароль должен быть минимум 5 символов').isLength({min: 5}),
     body('fullName', 'укажите имя').isLength({min: 3}),
-    body('avatarUrl', 'неверная ссылка на аватарку').optional().isURL(),
+    //body('avatarUrl', 'неверная ссылка на аватарку').optional().isURL(),
 ]
 
 export const autorizationValidation = [
